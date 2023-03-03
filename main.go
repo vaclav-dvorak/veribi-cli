ada58a383df6e951f24d6e7af70ea043782112c6
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2023-03-03 15:44:55 +0100

    refactor: Update all dependencies

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 // Package main implements Veribi CLI.
 package main
 
-import "github.com/vaclav-dvorak/veribi-cli/cmd/veribi"
+import (
+	"fmt"
+
+	"github.com/vaclav-dvorak/veribi-cli/cmd/veribi"
+)
+
+const (
+	blue  = "\033[94m"
+	reset = "\033[0m"
+)
+
+var (
+	logoSmall = []string{
+		`                 o|    o        |    o`,
+		` .    ,,---.,---..|---..   ,---.|    .`,
+		`  \  / |---'|    ||   ||---|    |    |`,
+		"   `'  `---'`    ``---'`   `---'`---'`",
+	}
+)
 
 func main() {
 	printWelcome()
 	veribi.Execute()
+}
+
+func printWelcome() {
+	for i := 0; i < len(logoSmall); i++ {
+		fmt.Printf("%s%s%s\n", blue, logoSmall[i], reset)
+	}
+	fmt.Println("")
 }
