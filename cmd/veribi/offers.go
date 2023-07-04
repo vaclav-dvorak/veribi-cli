8165d22ebcf0252bc1ab67c4fdeed9c079784907
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2023-07-04 10:49:31 +0200

    feat: Use go-pretty table instead of uitable

--- cmd/veribi/offers.go
+++ cmd/veribi/offers.go
@@ -2,10 +2,12 @@ package veribi
 
 import (
 	"fmt"
+	"os"
 	"sort"
 
 	"github.com/fatih/color"
-	"github.com/gosuri/uitable"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -45,12 +47,8 @@ var offersCmd = &cobra.Command{
 			})
 		}
 
-		t := uitable.New()
-		t.MaxColWidth = 80
-
-		t.AddRow("ℹ️", "ID", "TH price ($/TH)", "Hosting price ($/day)", "Health", "Title")
-		t.AddRow("", "--", "---------------", "---------------------", "------", "-----")
-
+		t := table.NewWriter()
+		t.AppendHeader(table.Row{"", "ID", "TH price ($/TH)", "Hosting ($/day)", "Health", "Title"})
 		for i := 0; i < len(offers); i++ {
 			hpf, hp := getHealth(offers[i])
 			style := color.New(color.FgGreen).SprintFunc()
@@ -60,13 +58,22 @@ var offersCmd = &cobra.Command{
 			case hpf < 0.8:
 				style = color.New(color.FgYellow).SprintFunc()
 			}
-			icon := "🫴"
+			iconStyle := color.New(color.FgGreen).SprintFunc()
+			icon := "Off"
 			if offers[i].Kind == "auction" {
-				icon = "💰"
+				iconStyle = color.New(color.FgBlue).SprintFunc()
+				icon = "Auc"
 			}
-			t.AddRow(icon, offers[i].ID, fmt.Sprintf("%6.2f", offers[i].ThsPrice), fmt.Sprintf("%4.2f", offers[i].HostPrice), style(fmt.Sprintf("%3.1d%%", hp)), offers[i].Title)
+			t.AppendRow(table.Row{iconStyle(icon), offers[i].ID, fmt.Sprintf("%6.2f", offers[i].ThsPrice), fmt.Sprintf("%4.2f", offers[i].HostPrice), style(fmt.Sprintf("%3.1d%%", hp)), offers[i].Title})
 		}
-		fmt.Println(t)
+		t.SetColumnConfigs([]table.ColumnConfig{
+			{Number: 3, Align: text.AlignRight},
+			{Number: 4, Align: text.AlignRight},
+			{Number: 5, Align: text.AlignRight},
+		})
+		t.SetOutputMirror(os.Stdout)
+		t.SetStyle(table.StyleLight)
+		t.Render()
 	},
 }
 
